67e2e33a4dfc1eaabbf424890573313c546c3590
rapidstylers-Project <rapidstylers@gmail.com>
2026-03-08 13:46:49 -0600

    fix: update CSP to allow Swagger UI

--- src/main/java/com/afrochow/config/SecurityConfig.java
+++ src/main/java/com/afrochow/config/SecurityConfig.java
@@ -282,8 +282,8 @@ public class SecurityConfig {
                 .contentSecurityPolicy(csp -> csp
                         .policyDirectives(
                                 "default-src 'self'; " +
-                                        "script-src 'self'; " +
-                                        "style-src 'self'; " +
+                                        "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+                                        "style-src 'self' 'unsafe-inline'; " +
                                         "img-src 'self' data: https:; " +
                                         "font-src 'self' data:; " +
                                         "connect-src 'self'; " +
